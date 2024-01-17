7fc112817e4ddbd711f51a20df2c24f635528d4d
Yuri Santos Sena Sousa <ysantoss@minsait.com>
2024-01-16 22:54:30 -0300

    edit: correcao script, versao pegando valor correto agora

--- Dockerfile/coleta-envs.sh
+++ Dockerfile/coleta-envs.sh
@@ -5,17 +5,17 @@ POM="pom.xml"
 # Busca as linhas contendo "br.com.fiap" no arquivo pom.xml
 REFERENCIA=$(grep -A 2 -m 1 "<groupId>br.com.fiap</groupId>" "$POM")
 
-# Extrai o valor de artefato da linha encontrada
+# Extrai o valor de parquimetro da linha encontrada
 ARTEFATO=$(echo "$REFERENCIA" | grep "<artifactId>" | sed -n 's/.*<artifactId>\(.*\)<\/artifactId>.*/\1/p')
 
-# Extrai o valor de versao da linha encontrada
-VERSAO=$(grep -m 1 "<version>" "$POM" | sed -n 's/.*<version>\(.*\)<\/version>.*/\1/p')
+# Extrai o valor de versao da linha abaixo da linha do artifactId
+VERSAO=$(grep -A 1 -m 1 "<artifactId>$ARTEFATO</artifactId>" "$POM" | grep "<version>" | sed -n 's/.*<version>\(.*\)<\/version>.*/\1/p')
 
 # Define as variáveis de ambiente
-export ARTEFATO
+export PARQUIMETRO
 export VERSAO
 
 # Exibe os valores encontrados
-echo "Artefato: $ARTEFATO"
+echo "Parquimetro: $ARTEFATO"
 echo "Versao: $VERSAO"
 
